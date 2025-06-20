93c7a27c8804f2d72c9f95691a027a8a0233b5bf
Russian95CrE <russian95@infernointeractive.win>
2025-06-20 00:02:51 -0300

    [FIX] Add error handling for 'make clean' in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ clear
 
 ISO_PATH="build/out/kernel.iso"
 
-make clean
+if ! make clean; then
+    echo -e "\n\e[31mTried to clean installation, but failed!\e[0m\n"
+    exit 1
+fi
+
 if ! make -j"$(nproc)"; then
     echo -e "\n\e[31mBuild failed!\e[0m\n"
     exit 1
